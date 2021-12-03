6ddd5bb2ffa8ba5a29e76a0db2289a706741db36
RamiroValle <rammirov14@gmail.com>
2021-12-02 23:06:37 -0300

    Fix sql and some compose parameter changes

--- functions/script/db_script.py
+++ functions/script/db_script.py
@@ -26,7 +26,7 @@ def main():
 def connect():
     db = mysql.connector.connect(host="db", port=3306, user="admin", password="admin", database="mysql")
     cr = db.cursor()
-    cr.execute("CREATE IF NOT EXISTS TABLE cars (id INT AUTO_INCREMENT PRIMARY KEY, plate varchar(9) NOT NULL UNIQUE, model varchar(255) NOT NULL)")
+    cr.execute("CREATE TABLE IF NOT EXISTS cars (id INT AUTO_INCREMENT PRIMARY KEY, plate varchar(9) NOT NULL UNIQUE, model varchar(255) NOT NULL)")
     db.commit()
     return db
     
